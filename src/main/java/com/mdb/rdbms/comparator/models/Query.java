9926dd2d59d69363826bb8718135688f72f6538c
Josh Smith <josh.smith@mongodb.com>
2025-07-02 12:17:03 -0500

    Updated the query model to fix the Show Queries function on the create pages

--- src/main/java/com/mdb/rdbms/comparator/models/Query.java
+++ src/main/java/com/mdb/rdbms/comparator/models/Query.java
@@ -3,8 +3,8 @@ package com.mdb.rdbms.comparator.models;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("logs")
-public record Query(String message, long millis, String loggerName, String location) {
-    public Query(String message, long millis, String loggerName){
-        this(message, millis, loggerName, "US");
+public record Query(String message, long millis, String source, String location) {
+    public Query(String message, long millis, String source){
+        this(message, millis, source, "US");
     }
 }
