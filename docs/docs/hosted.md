04c71fe528dd0d4e6f41e3c8c0dba54b90b43620
Josh Smith <josh.smith@mongodb.com>
2025-07-24 15:14:36 -0500

    Updated the docs to include a page with all of the hosting info so SA's have that available

---
+++ docs/docs/hosted.md
@@ -0,0 +1,25 @@
+---
+sidebar_position: 4
+---
+
+# Hosted Demo
+
+Mongo Logistics Offers a hosted demo for use by MongoDB employees. The demo can be found here [Mongo Logistics](https://rdbms-comparator.sa-demo.staging.corp.mongodb.com)
+
+
+## Environment
+
+To help provide clarity here is details on how the hosted demo is setup. 
+
+
+|             | MongoDB | Postgres                  |
+|-------------| ----- |---------------------------|
+| DB Platform | MongoDB Atlas | AWS RDS Postgres Instance |
+| Tier| M20 | db.t3.medium              |
+| Version | 8.0.6 | 16.8                      |
+| CPU         | 2 vCPU | 2 vCPU                    |
+| RAM         | 4 GB| 4GB                       | 
+| Disk        | 20GB | 200GB                     | 
+| IOPS        | 2000 | 3000                      | 
+| CSP         | AWS | AWS                       |
+| Region      | US-EAST-1 | US-EAST-1                 |
--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -5,48 +5,47 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  Img: React.ComponentType<React.ComponentProps<'img'>>;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    Img: require('@site/static/img/presentation.png').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Demo in the hosted environment (MDB Employees) or download and run locally. Pick the approach that works best for you.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Resonates with Customers',
+    Img: require('@site/static/img/coding.png').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Mongo Logistics goes beyond the simple app to leverage enterprise
+          frameworks to show the power of MDB with tools our customers use.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Transparent',
+    Img: require('@site/static/img/clear_communication.png').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Full source code is shared so that customers can view for themselves what's happening. Provides confidence that
+          the comparison is fair, and gives cusotmers the ability to use the content to educate themselves.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, Img, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img src={Img} className={styles.featureImg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
